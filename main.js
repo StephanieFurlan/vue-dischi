b955b45f048fba004768a3dfc3be971c14bf8153
Stephanie <stephanie.furlan94@gmail.com>
2020-12-17 17:17:29 +0100

    Filter by Genre

--- main.js
+++ main.js
@@ -2,18 +2,28 @@
 var app = new Vue({
    el:"#app",
    data: {
-      discs: []
+      discs: [],
+      genres: [],
+      selected: "All"
    },
    methods: {
+      chooseGenre: function() {
 
+      }
    },
    mounted: function() {
+      // lista dischi
       axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((response) => {
          this.discs = response.data.response;
+         this.discs.forEach(disc => {
+            // unique genres
+            if(!this.genres.includes(disc.genre)){
+               this.genres.push(disc.genre);
+            }
+         })
       })
-
    }
 
 })
