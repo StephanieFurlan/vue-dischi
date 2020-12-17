b996efdd4997ede4a2a94deec4558113af79f693
Stephanie <stephanie.furlan94@gmail.com>
2020-12-17 18:00:54 +0100

    Sort and filter layout

--- main.js
+++ main.js
@@ -3,11 +3,28 @@ var app = new Vue({
    el:"#app",
    data: {
       discs: [],
+      defaultDiscs: [],
       genres: [],
-      selected: "All"
+      selected: "All",
+      buttonText: "Sort By Year",
+      sorted: false
    },
    methods: {
-      chooseGenre: function() {
+      sortMe: function() {
+         this.sorted = !this.sorted;
+
+         if (this.sorted) {
+            this.buttonText = "Show By Importance";
+            this.discs.sort(function (a, b) {
+              return a.year - b.year;
+            });;
+
+         } else {
+            this.buttonText = "Sort By Year";
+            this.discs = this.defaultDiscs.map(disc => disc);
+            // this.discs.forEach(disc => console.log(disc.year));
+         }
+
 
       }
    },
@@ -17,6 +34,7 @@ var app = new Vue({
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((response) => {
          this.discs = response.data.response;
+         this.defaultDiscs = this.discs.map(disc => disc);
          this.discs.forEach(disc => {
             // unique genres
             if(!this.genres.includes(disc.genre)){
